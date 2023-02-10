2dc2bee766da4ba064deddf1f587192b93b4b6ca
lstar93 <starcu93@gmail.com>
2023-02-10 23:05:02 +0100

    removed unnecessary parameter

--- cli.py
+++ cli.py
@@ -324,7 +324,6 @@ class CLIData:
             self.executor.example(known_args.shape)
 
         self.parser.add_argument('--verbose', action='store_true')
-        self.parser.add_argument('--example', action='store_true')
         self.parser.add_argument('--to-file', type=str)
 
         self.executor.execute(known_args.shape)
